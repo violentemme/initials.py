2e6335a42e5a9e0601daed4631ba78b24610150a
violentemme <97240374+violentemme@users.noreply.github.com>
2022-04-11 13:10:59 -0500

    Create code.py
    But lo...my creation

---
+++ code.py
@@ -0,0 +1,14 @@
+# My name is Emily Wheaton
+# I'm like, hella good at MarioKart DoubleDash
+
+initials = '''
+EEEEE     w   W
+E         W   W
+E         W   W
+EEE       W W W
+E         WW WW
+E         WW WW
+EEEEE     W   W
+'''
+
+print(initials)
